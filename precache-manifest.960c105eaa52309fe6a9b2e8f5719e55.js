e2bee0f5ca32bacf37cb6b8639595de3cc083d5e
Julian De Tomasi <juliandetomasi@gmail.com>
2020-05-03 17:51:04 -0300

    Updates

---
+++ precache-manifest.960c105eaa52309fe6a9b2e8f5719e55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3098af643d676aa9de94e08a0bfec9fb",
+    "url": "/grocery-list/index.html"
+  },
+  {
+    "revision": "0e2ffbf513b4cd499be0",
+    "url": "/grocery-list/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e81ce7fe1c1937015e8d",
+    "url": "/grocery-list/static/js/2.5699e883.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/grocery-list/static/js/2.5699e883.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e2ffbf513b4cd499be0",
+    "url": "/grocery-list/static/js/main.99458c3e.chunk.js"
+  },
+  {
+    "revision": "52be9dfaefb67302e274",
+    "url": "/grocery-list/static/js/runtime-main.058846d8.js"
+  }
+]);
